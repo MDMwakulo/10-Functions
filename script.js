3a88f612f7cb040cc697f90f8293439f9e8e227e
MDMwakulo <mdmwakulo@gmail.com>
2023-06-24 15:33:23 +0300

    Coding Challenge #1

--- script.js
+++ script.js
@@ -112,6 +112,7 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 greetArr('Hello')('Durell'); // Hello Durell
 */
 
+/*
 // The call and apply Methods
 const lufthansa = {
   airline: 'Lufthansa',
@@ -210,3 +211,39 @@ const addTaxRate = function (rate) {
 const taxRate = addTaxRate(0.23);
 taxRate(100);
 taxRate(23);
+*/
+
+// Coding Challenge #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get the answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    // Update the answer
+    typeof answer === 'number' &&
+      answer >= 0 &&
+      answer <= this.answers.length &&
+      this.answers[answer]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}.`);
+    }
+  },
+};
+
+const pollBtn = document.querySelector('.poll');
+pollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
